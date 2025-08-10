0d99fd91831c3ada9499ab085825725b0c4890da
Murilo <murkuehne@gmail.com>
2025-08-09 22:10:00 -0300

    Dashboard Layout Done

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,19 @@
+import { Header } from "components"
+
 const dashboard = () => {
-  return <div>Dashboard Page</div>
+  const user = {
+    name: "John Doe",
+  }
+
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+    </main>
+  )
 }
 
 export default dashboard
